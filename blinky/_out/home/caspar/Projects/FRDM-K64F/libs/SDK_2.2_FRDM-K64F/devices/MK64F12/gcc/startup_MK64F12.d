f7e468d2ff1b691e70c40a60212d5cbf45a73e1f
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-08-12 21:31:48 +0200

    -

--- blinky/_out/home/caspar/Projects/FRDM-K64F/libs/SDK_2.2_FRDM-K64F/devices/MK64F12/gcc/startup_MK64F12.d
+++
@@ -1,2 +0,0 @@
-_out/home/caspar/Projects/FRDM-K64F/libs/SDK_2.2_FRDM-K64F/devices/MK64F12/gcc/startup_MK64F12.o: \
- /home/caspar/Projects/FRDM-K64F/libs/SDK_2.2_FRDM-K64F/devices/MK64F12/gcc/startup_MK64F12.S
--- blinky/src/main.cpp
+++ blinky/src/main.cpp
@@ -1,9 +1,13 @@
 #include <cstdlib>
 
+#include <SEGGER_RTT.h>
+
 #include "board.hpp"
 
 extern "C" int main() {
     board::init();
+
+    SEGGER_RTT_printf(0, "Test\n");
 
     for (;;) {
     }
