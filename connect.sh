048bf59a3e14a263cc4e96b59ca439e981174017
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-11-14 08:58:36 +0100

    Connection script

--- connect.sh
+++ connect.sh
@@ -1,1 +1,1 @@
-JLinkGDBServer -device MK64FN1M0xxx12 -if swd -speed auto -nogui -xc connect.gdb
+JLinkGDBServer -select usb=621000000 -device MK64FN1M0xxx12 -if swd -speed auto -nogui -xc connect.gdb
